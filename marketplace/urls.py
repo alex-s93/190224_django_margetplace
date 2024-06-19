9e2a93d77bf6e5154d7b61844e12a9cfc84ff7d9
VictoriaSpecht <spechtvictoria1@gmail.com>
2024-06-19 13:19:35 +0200

    add new endpoind for crud category

---
+++ marketplace/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from marketplace.views.category_views import CategoryViewSet
+
+
+router = DefaultRouter()
+router.register('categories', CategoryViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
